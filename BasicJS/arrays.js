f1e9179122c84428028006616ab1449af11e00e3
Erick-ViBe <erickvb12@gmail.com>
2020-07-19 20:43:53 -0500

    Update arrays.js

--- BasicJS/arrays.js
+++ BasicJS/arrays.js
@@ -23,4 +23,44 @@ console.log(frutas.shift());
 console.log(frutas.indexOf("Cereza"));
 //Devuelve la posicion del elemento
 
+var result = frutas.filter(fruta => fruta.length > 6);
+//.filter - Nos permite filtrar solo los elementos que deseamos(segun ciertos criterios), y nos devuelve un nuevo array. result = ["Manzana", "Platano"]
+
+var tamanio = frutas.map(function(x) {
+    return x.length;
+});
+//.map -o Crea un nuevo array con los resultados de la llamada a la funcion indicada aplicados a cada uno de sus elementos - tamanio = [7, 7, 6, 5]
+
+//Otro ejemplo de .map
+var numbers = [1, 4, 9];
+
+var roots = numbers.map(function(num) {
+    return Math.sqrt(num);
+});
+//Ahora roots es igual a [1, 2, 3]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
