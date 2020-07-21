347dc6758c3e20df8ae3f4e261f5ae1b1e2e43fa
Erick-ViBe <50604053+Erick-ViBe@users.noreply.github.com>
2020-07-21 16:46:01 +0000

    Update arrays.js

--- BasicJS/arrays.js
+++ BasicJS/arrays.js
@@ -53,5 +53,3 @@ frutasCortas = frutas.some(function(fruta){
     return fruta.length <= 5;
 });
 //Devuelve un true o un false de acuerdo a si existe un elemento en el array que cumpla con lo indicado, en este caso devolveria True ya que "fresa" tiene 5 letras
-
-//Este comentario se debe borrar
